8c2477e1cefa927e0aa471221450207b091e9d30
lyubomir_boradzhiev <lyubomir.boradzhiev@rewe-digital.com>
2018-10-05 16:04:02 +0300

    Working Factoriel, not so good countIntegerDigits

--- src/main/java/algorithms/basicmath/Basics.java
+++ src/main/java/algorithms/basicmath/Basics.java
@@ -9,5 +9,34 @@ package algorithms.basicmath;
   - task: Calculate GCD (https://en.wikipedia.org/wiki/Greatest_common_divisor), for given n and m
  */
 public class Basics {
+    public static void main(String[] args) {
+        System.out.println(countNumberOfDigits(555));
+        System.out.println("Facotrial is:  "+factorial(17));
+    }
 
+    public static String countNumberOfDigits(int providedInteger) {
+        String result = null;
+
+        if (providedInteger <= 9) {
+            result = "ther is 1 digit in the number";
+        } else if (providedInteger >= 10 && providedInteger <= 99) {
+            result = "ther is 2 digit in the number";
+        } else if (providedInteger >= 100 && providedInteger <= 999) {
+            result = "ther is 3 digit in the number";
+        } else if (providedInteger >= 1000 && providedInteger <= 9999) {
+            result = "ther is 3 digit in the number";
+        }
+
+        return result;
+    }
+
+    public static long factorial(int factorialNumber) {
+        long result = 1;
+
+        for (long i = factorialNumber; i >= 1; i--) {
+            result *= i;
+        }
+
+        return result;
+    }
 }
