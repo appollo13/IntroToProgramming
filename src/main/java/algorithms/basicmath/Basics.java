ced1ede7e52cc5c0c7ff40e9bccd4900ae975b7d
emko-pc\emko <gabro_81@yahoo.com>
2018-10-11 20:18:51 +0300

    Basic math tasks

---
+++ src/main/java/algorithms/basicmath/Basics.java
@@ -0,0 +1,37 @@
+package algorithms.basicmath;
+
+/*
+4 (1.1.1) Basic mathematical concepts
+
+  - task: Count the number of digits in an integer, for a given integer
+  - task: Calculate n! (https://en.wikipedia.org/wiki/Factorial), for a given n
+  - task: Calculate LCD (https://en.wikipedia.org/wiki/Lowest_common_denominator), for given n and m
+  - task: Calculate GCD (https://en.wikipedia.org/wiki/Greatest_common_divisor), for given n and m
+ */
+public class Basics {
+
+    public static final int[] PRIMES = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+            67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+            179, 181, 191, 193, 197, 199};
+
+    public static int countNumberOfDigits(int providedInteger) {
+        //TODO
+        return -1;
+    }
+
+    public static long factorial(int n) {
+        //TODO
+        return -1;
+    }
+
+    public static int lowestCommonDivisor(int firstNumber, int secondNumber) {
+        //TODO
+        return -1;
+    }
+
+    public static int greatestCommonDivisor(int firstNumber, int secondNumber) {
+        //TODO
+        return -1;
+    }
+}
+
---
+++ src/test/java/algorithms/basicmath/BasicTest.java
@@ -0,0 +1,10 @@
+package algorithms.basicmath;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class BasicTest {
+
+    //TODO
+}
