b243d5730cd34ce3922038580fd79a2a51880fa8
lyubomir.boradzhiev <lyubomir.boradzhiev@rewe-digital.com>
2018-12-17 10:07:27 +0200

    recursion 1.0

--- src/main/java/algorithms/basicmath/Recursion.java
+++ src/main/java/algorithms/basicmath/Recursion.java
@@ -11,14 +11,30 @@ package algorithms.basicmath;
 public class Recursion {
 
     public static long factorial(int n) {
-        //TODO
-        return -1;
+
+
+        if (n >= 2) {
+            long l = n * factorial(n - 1);
+            System.out.println(l);
+            return l;
+        }
+        return 1;
+
     }
 
-    public static long fibonacci(int n) {
-        //TODO
-        return -1;
+    public static long fibonacci(long n) {
+
+        long p = 0;
+        while (p < n){
+            long sum = p + fibonacci(n-p);
+            p++;
+
+        }
+
+        return 0;
     }
+
+
 
     public static int lowestCommonDivisor(int firstNumber, int secondNumber) {
         //TODO
